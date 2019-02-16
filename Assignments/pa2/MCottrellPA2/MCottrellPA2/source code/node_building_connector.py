46d194fd486c1653c726bd9c8aabdb8a2da7add5
Michael Cottrell <runksmania1011@gmail.com>
2019-02-15 20:26:43 -0800

    Update node_building_connector.py
    Created logic for constructor to setup hash table for the node corresponding to the building code.
    Created a function to lookup node information based on building code.

--- Assignments/pa2/MCottrellPA2/MCottrellPA2/source code/node_building_connector.py
+++ Assignments/pa2/MCottrellPA2/MCottrellPA2/source code/node_building_connector.py
@@ -2,4 +2,21 @@ from csv_processor import process_csv
 
 class NodeBuildingConnector:
     def __init__(self, file_name):
-        self._designations = process_csv(file_name)
+        designations_array = process_csv(file_name)
+        
+        self._designations = {}
+        
+        for row in designations_array:
+            
+            #Make sure we aren't using info line.
+            if row[0] != "Node Name":
+                
+                #Add the following to the designation hash table.
+                #    1. A lookup key which the building code.
+                #    2. A tuple containing:
+                #        I. The node associated with the building.
+                #        II. The name of the building.
+                self._designations[row[1]] = (row[0], row[3])
+             
+    def building_lookup(build_code):
+        return self._designations[build_code]
