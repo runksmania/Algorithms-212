5f82b28cfd70bec14eb672635eaa17aec13ca338
Michael Cottrell <runksmania1011@gmail.com>
2019-03-10 20:05:34 -0700

    Deleted main_test.py as it is no longer needed. Added logic to main to test all possible start points and find the minimal route. This logic meets the requirements for a tier 3 submission. Commenting and Output formatting for PA3 curriculum still needs to be done.

--- Assignments/pa3/MichaelCottrellPA3/MichaelCottrellPA3/sourceCode/main.py
+++ Assignments/pa3/MichaelCottrellPA3/MichaelCottrellPA3/sourceCode/main.py
@@ -1,3 +1,4 @@
+import sys
 from csv_processor import process_csv
 from graph import Graph
 from heapq import *
@@ -12,26 +13,41 @@ deliveries = {}
 for i in delivery_data:
   deliveries[i[0]] = 0
 
-start = delivery_data[0][0]
-deliveries[start] = 1
-
 for i in map_data:
   g.add_vertex(i[0])
   g.connect_vertex(i[0], i[1], i[2], True)
 
-total_time = 0
+total_time = sys.maxsize
 path = {}
+start = ''
 
-current = start
+for i in delivery_data:
+  current_start = i[0]
+  delivery_current = deliveries.copy()
+  delivery_current[current_start] = 1
+  current = current_start
+  delivery_time = 0
+  current_path = {}
 
-while len(path) < len(deliveries) - 1:
-  closest = g.shortest_path_dict_nodes(current, deliveries)
-  total_time += closest[0]
-  path[current] = closest[1][1]
-  current = closest[1][0]
-  deliveries[current] = 1
+  while len(current_path) < len(delivery_current) - 1:
+    closest = g.shortest_path_dict_nodes(current, delivery_current)
+    
+    if closest != 0:
+      delivery_time += closest[0]
+      current_path[current] = closest[1][1]
+      current = closest[1][0]
+      delivery_current[current] = 1
+    else:
+      break
+  
+  if delivery_time < total_time:
+    total_time = delivery_time
+    path = current_path
+    start = current_start
+  
 
 print(path)
+print(total_time)
 print(start + ' -> ' + ' -> '.join(path[start].split()))
 for i in path:
     if i != start:
