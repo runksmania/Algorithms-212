6f9972097a95d5db4e81b71c22457bec84ca9853
Michael Cottrell <runksmania1011@gmail.com>
2019-03-10 22:32:20 -0700

    Finished formatting output for PA3 guidelines.  Also logic and output to show how much time it takes to go to each step in the path.

--- Assignments/pa3/MichaelCottrellPA3/MichaelCottrellPA3/sourceCode/main.py
+++ Assignments/pa3/MichaelCottrellPA3/MichaelCottrellPA3/sourceCode/main.py
@@ -3,7 +3,8 @@ from csv_processor import process_csv
 from graph import Graph
 from heapq import *
 
-print('***Route Planner***\nNote all input files will assumed to be in the resourceFiles directory.')
+print('***Route Planner***')
+print('*Note all input files will assumed to be in the /resourceFiles directory.\n*Enter the file name only.\n')
 map_data = process_csv('resourceFiles/' + input('Enter map file: '))
 delivery_data = process_csv('resourceFiles/' + input('Enter delivery file: '))
 
@@ -34,7 +35,7 @@ for i in delivery_data:
     
     if closest != 0:
       delivery_time += closest[0]
-      current_path[current] = closest[1][1]
+      current_path[current] = (closest[0], closest[1][1])
       current = closest[1][0]
       delivery_current[current] = 1
     else:
@@ -46,9 +47,8 @@ for i in delivery_data:
     start = current_start
   
 
-print(path)
-print(total_time)
-print(start + ' -> ' + ' -> '.join(path[start].split()))
+print('\nTotal transit time: ' + str(total_time) + ' minutes')
+print('Complete Route:\n\nTime to first desination: ' + str(path[start][0]) + ' minutes\nRoute: ' + start + ' -> ' + ' -> '.join(path[start][1].split()))
 for i in path:
     if i != start:
-        print(i + ' -> ' + ' -> '.join(path[i].split()))
+        print('Time to next desination: ' + str(path[i][0]) + ' minutes\nRoute: ' + i + ' -> ' + ' -> '.join(path[i][1].split()) + '\n')
