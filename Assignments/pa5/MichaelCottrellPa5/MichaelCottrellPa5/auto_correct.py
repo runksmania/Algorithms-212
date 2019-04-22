147c17d90457a3f27a2978e7304a42816cffdf63
Michael Cottrell <runksmania1011@gmaill.com>
2019-04-22 13:35:13 -0700

    Changed autocorrect from a class to a file containing a static function. Added function to find possible matches in autocorrect.
    Added levenshtein edit distance.
    
    Added to main to test find_possible_matches.

--- Assignments/pa5/MichaelCottrellPa5/MichaelCottrellPa5/auto_correct.py
+++ Assignments/pa5/MichaelCottrellPa5/MichaelCottrellPa5/auto_correct.py
@@ -1,3 +1,37 @@
-class auto_correct:
+from levenshtein import calculate_edit_distance
+from dictionary import Dictionary
+from heapq import *
 
-    def __init__(self, ):
+#Returns a pq of the possible matches for a given word in the given dictionary.
+def find_possible_matches(incorrect_word, dictionary):
+  
+  possible_matches = []
+
+  if incorrect_word != '':
+       
+    words = dictionary.lookup_words_starting_with(incorrect_word[0])
+
+    for i in words:
+      dist = calculate_edit_distance(incorrect_word, i)
+
+      #Otherwise push into pq the distance and the word.
+      heappush(possible_matches, (dist, i))
+
+    if len(incorrect_word) > 1:
+      #Repeat earlier step with first two letters in the word.
+
+      words = dictionary.lookup_words_starting_with(incorrect_word[:1])
+
+      for i in words:
+        dist = calculate_edit_distance(incorrect_word, i)
+        heappush(possible_matches, (dist, i))
+
+      #Now check if first letter was typed incorrectly.
+
+      words = dictionary.lookup_words_starting_with(incorrect_word[1])
+
+      for i in words:
+        dist = calculate_edit_distance(incorrect_word, i)
+        heappush(possible_matches, (dist, i))
+
+  return possible_matches
--- Assignments/pa5/MichaelCottrellPa5/MichaelCottrellPa5/main.py
+++ Assignments/pa5/MichaelCottrellPa5/MichaelCottrellPa5/main.py
@@ -1,10 +1,23 @@
 from dictionary import Dictionary
+from auto_correct import find_possible_matches
+from heapq import heappop
 
 dictionary = Dictionary('resourceFiles/words.txt')
 
-dictionary.print_words_starting_with('ph')
+#li = dictionary.lookup_words_starting_with('this')
 
-li = dictionary.lookup_words_starting_with('ph')
+#for i in li:
+ # print(i)
 
-for i in li:
+li = find_possible_matches('philosoph', dictionary)
+
+ten_closest_matches = []
+
+while len(ten_closest_matches) < 10:
+  top = heappop(li)
+
+  if top[1] not in ten_closest_matches:
+    ten_closest_matches.append(top[1])
+
+for i in ten_closest_matches:
   print(i)
