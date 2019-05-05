7b9b07c0dd4d5697f3e3be5cccc6a2e8f17c9723
Michael Cottrell <runksmania1011@gmail.com>
2019-05-04 22:26:33 -0700

    Create hamming.py
    Created a hamming file with and created an encode method.

---
+++ Assignments/PA6/hamming.py
@@ -0,0 +1,23 @@
+encoding_matrix = [
+  [1,0,0,0,1,1,0],
+  [0,1,0,0,1,0,1],
+  [0,0,1,0,0,1,1],
+  [0,0,0,1,1,1,1]
+]
+
+#Function to encode a list of bits into a hamming algorithm.
+#Performs matrix multiplication on bits_to_encode and the encoding_matrix.
+#Returns a list of bits that is the encoded bits.
+def encode(bits_to_encode):
+
+  matrix = [[] for i in range(len(encoding_matrix[0]))]
+
+  if len(bits_to_encode) == len(encoding_matrix):
+    for i in range(len(encoding_matrix)):
+      l = encoding_matrix[i]
+
+      for j in range(len(l)):
+        matrix[j].append(bits_to_encode[i] * l[j])
+
+  print(matrix)
+  return [sum(i) % 2 for i in matrix]
